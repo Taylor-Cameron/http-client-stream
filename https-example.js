359b4874bf35c8c061aebfa1b427db0dc98332f0
Taylor-Cameron <taylorcameron95@hotmail.ca>
2018-10-28 18:16:20 +0000

    request to www.example.com added

--- https-example.js
+++ https-example.js
@@ -1,3 +1,15 @@
 var https = require("https");
+var options = {
+  host: 'www.example.com',
+  path: '/'
+}
 
-console.log('I did it!');
+var callback = function() {
+  console.log("In response handler callback")
+}
+
+console.log('I\'m about to make the request');
+
+https.request(options, callback).end();
+
+console.log('I\'ve made the request');
